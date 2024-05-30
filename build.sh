059dee47615a61b54816afc5afe05533ff782670
benjilli <benjilli@tencent.com>
2024-05-30 18:06:19 +0800

    chore: update github action config

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-npm run build-cli -- ai.md --base /ai/ --out ./dist/ai/
-npm run build-cli -- slides.md --base /slides/ --out ./dist/slides/
+rm -rf ./dist/
+
+if [ "$1" ]; then
+  npm run build-cli -- ai.md --base "/$1/ai/" --out ./dist/ai/
+  npm run build-cli -- slides.md --base "/$1/slides/" --out ./dist/slides/
+else
+  npm run build-cli -- ai.md --base /ai/ --out ./dist/ai/
+  npm run build-cli -- slides.md --base /slides/ --out ./dist/slides/
+fi
