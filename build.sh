6d887c3a981b72a648f0c32839af479946f2c869
benjilli <benjilli@tencent.com>
2024-06-14 20:22:12 +0800

    chore: fix headless-ui build script config

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ rm -rf ./dist/
 if [ "$1" ]; then
   npm run build-cli -- ai.md --base "/$1/ai/" --out ./dist/ai/
   npm run build-cli -- slides.md --base "/$1/slides/" --out ./dist/slides/
-  npm run build-cli -- headless-ui.md --base "/$1/headless-ui/" --out ./dist/slides/
+  npm run build-cli -- headless-ui.md --base "/$1/headless-ui/" --out ./dist/headless-ui/
 else
   npm run build-cli -- ai.md --base /ai/ --out ./dist/ai/
   npm run build-cli -- slides.md --base /slides/ --out ./dist/slides/
-  npm run build-cli -- headless-ui.md --base /headless-ui/ --out ./dist/slides/
+  npm run build-cli -- headless-ui.md --base /headless-ui/ --out ./dist/headless-ui/
 fi
